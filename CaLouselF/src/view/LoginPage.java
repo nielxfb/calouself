e768d64f45628dcea7a51eca4e7b9c9d19b755fd
Daniel Adamlu <113871018+nielxfb@users.noreply.github.com>
2024-12-09 23:30:52 +0700

    feat: login logic

--- CaLouselF/src/view/LoginPage.java
+++ CaLouselF/src/view/LoginPage.java
@@ -1,29 +1,47 @@
 package view;
 
+import abstraction.Response;
+import controller.UserController;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import middleware.AuthMiddleware;
+import model.User;
+import util.SessionManager;
 import util.StageManager;
 
 public class LoginPage extends VBox {
 	
-	private Label loginLbl;
-	private Label usernameLbl, passwordLbl;
+	private Label title;
+	private Label usernameLbl, passwordLbl, errorLbl;
 	private TextField usernameTf;
 	private PasswordField passwordTf;
 	private Button loginBtn;
 	private Hyperlink registerLink;
 	
 	public LoginPage() {
+		middleware();
 		initPage();
 		addEvent();
 	}
 	
+	private void middleware() {
+		if (AuthMiddleware.loggedIn()) {
+			StageManager st = StageManager.getInstance(null);
+			st.getStage().getScene().setRoot(new HomePage());
+		}
+	}
+	
 	private void initPage() {
-		loginLbl = new Label("Login Page");
+		title = new Label("Login Page");
+		title.setFont(Font.font("Verdana", FontWeight.BOLD, 22));
+		VBox.setMargin(title, new Insets(0, 0, 15, 0));
 		
 		usernameLbl = new Label("Username");
 		passwordLbl = new Label("Password");
@@ -31,13 +49,31 @@ public class LoginPage extends VBox {
 		usernameTf = new TextField();
 		passwordTf = new PasswordField();
 		
+		errorLbl = new Label("");
+		errorLbl.setStyle("-fx-text-fill: red");
+		
 		loginBtn = new Button("Login");
 		registerLink = new Hyperlink("Register here.");
+		
+		this.getChildren().addAll(title, usernameLbl, usernameTf, passwordLbl, passwordTf, errorLbl, loginBtn, registerLink);
+		
+		this.setPadding(new Insets(20));
+		this.setSpacing(5);
 	}
 	
 	private void addEvent() {
 		loginBtn.setOnMouseClicked(e -> {
-			// Implement login logic.
+			String username = usernameTf.getText();
+			String password = passwordTf.getText();
+			Response<User> response = UserController.login(username, password);
+			if (response.isSuccess) {
+				SessionManager session = SessionManager.getInstance();
+				session.setUser(response.data);
+				StageManager st = StageManager.getInstance(null);
+				st.getStage().getScene().setRoot(new HomePage());
+			} else {
+				errorLbl.setText(response.message);
+			}
 		});
 		
 		registerLink.setOnMouseClicked(e -> {
