633967ba829037f20b8606957ffbe264426d6858
Daniel Adamlu <adamludnl@gmail.com>
2024-12-13 22:13:06 +0700

    feat: delete item page

--- src/view/DeleteItemPage.java
+++ src/view/DeleteItemPage.java
@@ -1,9 +1,28 @@
 package view;
 
 import abstraction.Page;
+import abstraction.Response;
+import controller.ItemController;
+import javafx.collections.FXCollections;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import middleware.AuthMiddleware;
+import model.Item;
+import util.AlertManager;
+import util.StageManager;
+
+import java.util.ArrayList;
 
 public class DeleteItemPage extends Page<BorderPane> {
+
+    private ItemTable table;
+    private Item selectedItem;
+    private Button delBtn;
 
     public DeleteItemPage() {
         super(new BorderPane());
@@ -14,16 +33,54 @@ public class DeleteItemPage extends Page<BorderPane> {
 
     @Override
     public void middleware() {
-
+        if (!AuthMiddleware.loggedIn()) {
+            StageManager st = StageManager.getInstance(null);
+            st.getStage().getScene().setRoot(new LoginPage().layout);
+        }
     }
 
     @Override
     public void initPage() {
+        Label title = new Label("Delete Item");
+        layout.setTop(new SellerNavbar());
 
+        title.setFont(Font.font("Verdana", FontWeight.BOLD, 22));
+
+        VBox vb = new VBox();
+        layout.setCenter(vb);
+
+        vb.getChildren().add(title);
+
+        vb.setPadding(new Insets(20));
+
+        table = new ItemTable();
+        delBtn = new Button("Delete");
+
+        vb.getChildren().addAll(table, delBtn);
+        vb.setSpacing(10);
+
+        Response<ArrayList<Item>> response = ItemController.getApproved();
+        if (response.isSuccess) {
+            table.setItems(FXCollections.observableArrayList(response.data));
+        } else {
+            table.setPlaceholder(new Label(response.message));
+        }
     }
 
     @Override
     public void addEvent() {
+        table.setOnMouseClicked(e -> {
+            selectedItem = table.getSelectionModel().getSelectedItem();
+        });
 
+        delBtn.setOnAction(e -> {
+            Response<Item> response = ItemController.deleteItem(selectedItem);
+            if (response.isSuccess) {
+                AlertManager.showSuccess(response.message);
+                table.getItems().remove(selectedItem);
+            } else {
+                AlertManager.showError(response.message);
+            }
+        });
     }
 }
