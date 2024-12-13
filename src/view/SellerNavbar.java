28b30033d6acb896ae919f908c990327b1bf537e
Daniel Adamlu <adamludnl@gmail.com>
2024-12-13 22:13:00 +0700

    feat: delete menu and minor rearrangements

--- src/view/SellerNavbar.java
+++ src/view/SellerNavbar.java
@@ -12,24 +12,27 @@ public class SellerNavbar extends MenuBar {
         Menu home = new Menu("Home");
         MenuItem homePage = new MenuItem("Home Page");
         MenuItem uploadItem = new MenuItem("Upload Item");
+        MenuItem deleteItem = new MenuItem("Delete Item");
         MenuItem logout = new MenuItem("Logout");
+        home.getItems().addAll(homePage, uploadItem, deleteItem, logout);
 
-        home.getItems().addAll(homePage, uploadItem, logout);
+        StageManager st = StageManager.getInstance(null);
 
         homePage.setOnAction(e -> {
-            StageManager st = StageManager.getInstance(null);
             st.getStage().getScene().setRoot(new HomePage().layout);
         });
 
         uploadItem.setOnAction(e -> {
-            StageManager st = StageManager.getInstance(null);
             st.getStage().getScene().setRoot(new UploadItemPage().layout);
+        });
+
+        deleteItem.setOnAction(e -> {
+            st.getStage().getScene().setRoot(new DeleteItemPage().layout);
         });
 
         logout.setOnAction(e -> {
             SessionManager session = SessionManager.getInstance();
             session.logout();
-            StageManager st = StageManager.getInstance(null);
             st.getStage().getScene().setRoot(new LoginPage().layout);
         });
 
