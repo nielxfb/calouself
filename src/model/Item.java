6b687123dafe35c95f9a234352041b3d5f5e0922
Daniel Adamlu <adamludnl@gmail.com>
2024-12-13 15:26:39 +0700

    feat: item model

---
+++ src/model/Item.java
@@ -0,0 +1,79 @@
+package model;
+
+import util.Connect;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class Item {
+
+    private String itemId;
+    private String itemName;
+    private String itemCategory;
+    private String itemSize;
+    private Integer itemPrice;
+
+    public Item(String itemId, String itemName, String itemCategory, String itemSize, Integer itemPrice) {
+        this.itemId = itemId;
+        this.itemName = itemName;
+        this.itemCategory = itemCategory;
+        this.itemSize = itemSize;
+        this.itemPrice = itemPrice;
+    }
+
+    public static ArrayList<Item> getAll() {
+        String query = "SELECT * FROM items";
+        ArrayList<Item> items = new ArrayList<>();
+        Connect db = Connect.getConnection();
+        try {
+            ResultSet rs = db.executeQuery(query);
+            while (rs.next()) {
+                Item item = new Item(rs.getString("item_id"), rs.getString("item_name"), rs.getString("item_category"), rs.getString("item_size"), rs.getInt("item_price"));
+                items.add(item);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return items;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getItemCategory() {
+        return itemCategory;
+    }
+
+    public void setItemCategory(String itemCategory) {
+        this.itemCategory = itemCategory;
+    }
+
+    public String getItemSize() {
+        return itemSize;
+    }
+
+    public void setItemSize(String itemSize) {
+        this.itemSize = itemSize;
+    }
+
+    public Integer getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(Integer itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+}
